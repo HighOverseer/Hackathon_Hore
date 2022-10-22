22eb78399eca2361e813d075e06dbe8f9b56e2ec
lawakobadu <73542361+lawakobadu@users.noreply.github.com>
2022-10-22 22:37:02 +0700

    tes

--- app/src/main/java/com/example/hackathon_hore/LoginActivity.kt
+++ app/src/main/java/com/example/hackathon_hore/LoginActivity.kt
@@ -3,14 +3,27 @@ package com.example.hackathon_hore
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val textDaftar = findViewById<TextView>(R.id.textDaftar)
         val tv_lupaPassword = findViewById<TextView>(R.id.tv_lupaPassword)
+
+        textDaftar.setOnClickListener {
+            Intent(this@LoginActivity, RegisterActivity::class.java).also {
+                startActivity(it)
+            }
+        }
 
         tv_lupaPassword.setOnClickListener {
             Intent(this@LoginActivity, ResetPasswordActivity::class.java).also{
---
+++ app/src/main/java/com/example/hackathon_hore/User.java
@@ -0,0 +1,12 @@
+package com.example.hackathon_hore;
+
+public class User {
+    public String nama_toko, no_hp;
+    public User(){
+
+    }
+    public User(String nama_toko, String no_hp){
+        this.nama_toko = nama_toko;
+        this.no_hp = no_hp;
+    }
+}
