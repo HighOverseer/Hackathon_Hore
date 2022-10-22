f6c4ad54396336c033b57896533fe67c308969a9
Alif Abdul Rauf <2011522024_alif@student.unand.ac.id>
2022-10-22 14:37:22 +0700

    profil

---
+++ app/src/main/java/com/example/hackathon_hore/ProfilActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hackathon_hore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hackathon_hore.R
+
+class ProfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
